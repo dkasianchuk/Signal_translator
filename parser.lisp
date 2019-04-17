6961a741834bad2fc3e535c994f9bf5c7fbf07fa
dkasyanchuk <dimo4ik9977@gmail.com>
2019-04-17 05:00:50 +0300

    Done

--- parser.lisp
+++ parser.lisp
@@ -1,37 +1,5 @@
 (load "scanner.lisp")
 
-(defvar *index* 0)
-
-(defun inc () (incf *index*))
-
-(defun tree-to-graph (source-file &optional (target-file "graph.dot"))
-  (let ((graph (cl-graph:make-graph 'cl-graph:dot-graph
-				    :default-edge-type :directed)))
-    (make-pairs (parse-file source-file)
-	        (lambda (pair)
-		  (let ((vertex1 (cl-graph:add-vertex graph
-						      (car (car pair))
-						      :dot-attributes (list :label (second (car pair)))))
-			(vertex2 (cl-graph:add-vertex graph
-						      (car (second pair))
-						      :dot-attributes (list :label (second (second pair))))))
-		    (cl-graph:add-edge-between-vertexes graph
-							vertex1
-							vertex2
-							:dot-attributes '(:label "")))))
-    (cl-graph:graph->dot graph target-file)))
-
-(defun make-pairs (lst fn)
-  (let ((index *index*))
-    (mapc (lambda (elem)
-	    (inc)
-	    (funcall fn (if (listp elem)
-			    (prog1
-				(list (list index (car lst)) (list *index* (car elem)))
-			      (make-pairs elem fn))
-			    (list (list index (car lst)) (list *index* elem)))))
-	  (cdr lst))))
-
 (defun parse-file (filename)
   (parser (scanner filename)))
 
@@ -54,50 +22,56 @@
 (defun <signal-program> ()
   (let ((program (<program>)))
     (if (and program (null *lexem-row*))
-	(list 'signal-program program)
+	(list '<signal-program> program)
 	(warn "ERROR: Expected end of file."))))
 
 (defun <program> ()
-  (if (is-correct-lexem (scan) 'keyword 'PROGRAM)
-      (let ((procedure-identifier (<procedure-identifier> (scan))))
-	(if procedure-identifier
-	    (if (is-correct-lexem (scan) 'delimeter 'semicolon)
-		(let ((block_ (<block>)))
-		  (if block_
+  (let ((start (scan)))
+    (if (is-correct-lexem start 'keyword 'PROGRAM)
+	(let ((procedure-identifier (<procedure-identifier> (scan))))
+	  (when procedure-identifier
+	    (let ((ts (scan)))
+	      (if (is-correct-lexem ts 'delimeter 'semicolon)
+		  (let ((block_ (<block>)))
+		    (when block_
 		      (if (is-correct-lexem (scan) 'delimeter 'dot)
-			  (list 'program 'PROGRAM procedure-identifier block_ 'dot)
-			  (warn "ERROR: dot expected"))
-		      (warn "ERROR: <BLOCK> error.")))
-		(warn "ERROR: ; expected."))
-	    (warn "ERROR: <PROCEDURE-IDENTIFIER> ERROR.")))
-      (warn "ERROR: Keyword 'PROGRAM' expected.")))
+			  (list '<program> 'PROGRAM procedure-identifier block_)
+			  (warn "ERROR: dot expected at the end"))))
+		  (warn "ERROR: ; expected at line: ~S, column ~S."
+			(lexem-row (second ts))
+			(lexem-column (second ts)))))))
+	(warn "ERROR: Keyword 'PROGRAM' expected at line: ~S, column ~S."
+	      (lexem-row (second start))
+	      (lexem-column (second start))))))
 
 (defun <block> ()
   (let ((declarations (<declarations>)))
-    (if declarations
-	(if (is-correct-lexem (scan) 'keyword 'BEGIN)
+    (when declarations
+      (let ((begin (scan)))
+	(if (is-correct-lexem begin 'keyword 'BEGIN)
 	    (let ((statements-list (<statements-list>)))
-	      (if statements-list
-		  (if (is-correct-lexem (scan) 'keyword 'END)
-		      (list 'block declarations 'BEGIN statements-list 'END)
-		      (warn "ERROR: Expected keyword 'END'"))
-		  (warn "ERROR: <STATEMETS-LIST> error.")))
-	    (warn "ERROR: Expected keyword 'BEGIN'."))
-	(warn "ERROR: <DECLARATIONS> error."))))
+	      (when statements-list
+		(let ((end (scan)))
+		  (if (is-correct-lexem end 'keyword 'END)
+		      (list '<block> declarations 'BEGIN statements-list 'END)
+		      (warn "ERROR: Expected keyword 'END' at line: ~S, column ~S."
+			    (lexem-row (second end))
+			    (lexem-column (second end)))))))
+	    (warn "ERROR: Expected keyword 'BEGIN' at line: ~S, column ~S."
+		  (lexem-row (second begin))
+		  (lexem-column (second begin))))))))
 
 (defun <declarations> ()
   (let ((constant-declarations (<constant-declarations>)))
-    (if constant-declarations
-	(list 'declarations constant-declarations)
-	(warn "ERROR: <CONSTANT-DECLARATIONS> error."))))
+    (when constant-declarations
+      (list '<declarations> constant-declarations))))
 
 (defun <constant-declarations> ()
   (let ((ts (scan)))
     (if (is-correct-lexem ts 'keyword 'CONST)
 	(let ((constant-declaration-list (<constant-declaration-list>)))
-	  (if constant-declaration-list
-	      (list 'constant-declarations 'CONST constant-declaration-list)
-	      (warn "ERROR: <CONSTANT-DECLARATION-LIST> error.")))
+	  (when constant-declaration-list
+	    (list '<constant-declarations> 'CONST constant-declaration-list)))
 	(prog2
 	    (unscan ts)
 	    (list 'constant-declarations)))))
@@ -108,10 +82,9 @@
 	       (if (is-correct-lexem ts 'keyword 'BEGIN)
 		   (unscan ts)
 		   (let ((constant-declaration (<constant-declaration> ts)))
-		     (if constant-declaration
-			 (cons constant-declaration (%constant-declaration-list))
-			 (warn "ERROR: <CONSTANT-DECLARATION> error.")))))))
-    (cons 'constant-declaration-list (%constant-declaration-list))))
+		     (when constant-declaration
+		       (cons constant-declaration (%constant-declaration-list))))))))
+    (cons '<constant-declaration-list> (%constant-declaration-list))))
 
 (defun <statements-list> ()
   (labels ((%statement-list ()
@@ -119,76 +92,85 @@
 	       (if (is-correct-lexem ts 'keyword 'END)
 		   (unscan ts)
 		   (let ((statement (<statement> ts)))
-		     (if statement
-			 (cons statement (%statement-list))
-			 (warn "ERROR: <STATEMENT> error.")))))))
-    (cons 'statements-list (%statement-list))))
+		     (when statement
+		       (cons statement (%statement-list))))))))
+    (cons '<statements-list> (%statement-list))))
 
 (defun <constant-declaration> (ts)
   (let ((constant-identifier (<constant-identifier> ts)))
-    (if constant-identifier
-	(if (is-correct-lexem (scan) 'delimeter 'eq-constant)
+    (when constant-identifier
+      (let ((eq-constant (scan)))
+	(if (is-correct-lexem eq-constant 'delimeter 'eq-constant)
 	    (let ((constant (<constant> (scan))))
-	      (if constant
-		  (if (is-correct-lexem (scan) 'delimeter 'semicolon)
-		      (list 'constant-declaration constant-identifier 'eq-constant constant)
-		      (warn "ERROR: ; expected."))
-		  (warn "ERROR: <CONSTANT> error.")))
-	    (warn "ERROR: ':=' expected"))
-	(warn "ERORR: <CONSTANT-IDENTIFIER> erorr."))))
+	      (when constant
+		(let ((semi (scan)))
+		  (if (is-correct-lexem semi 'delimeter 'semicolon)
+		      (list '<constant-declaration> constant-identifier 'eq-constant constant)
+		      (warn "ERROR: ; expected at line ~S, column ~S."
+			    (lexem-row (second semi))
+			    (lexem-column (second semi)))))))
+	    (warn "ERROR: ':=' expected at line ~S, column ~S."
+		  (lexem-row (second eq-constant))
+		  (lexem-column (second eq-constant))))))))
 
 (defun <statement> (ts)
   (let ((variable-identifier (<variable-identifier> ts)))
-    (if variable-identifier
-	(if (is-correct-lexem (scan) 'delimeter 'eq-variable)
+    (when variable-identifier
+      (let ((eq-variable (scan)))
+	(if (is-correct-lexem eq-variable 'delimeter 'eq-variable)
 	    (let ((constant (<constant> (scan))))
-	      (if constant
-		  (if (is-correct-lexem (scan) 'delimeter 'semicolon)
-		      (list 'statement variable-identifier 'eq-variable constant)
-		      (warn "ERROR: ; expected."))
-		  (warn "ERROR: <CONSTANT> erorr.")))
-	    (warn "ERROR: ':=' expected"))
-	(warn "ERORR: <VARIABLE-IDENTIFIER> erorr."))))
+	      (when constant
+		(let ((semi (scan)))
+		  (if (is-correct-lexem semi 'delimeter 'semicolon)
+		      (list '<statement> variable-identifier 'eq-variable constant)
+		      (warn "ERROR: ; expected at line ~S,column ~S."
+			    (lexem-row (second semi))
+			    (lexem-column (second semi)))))))
+	    (warn "ERROR: ':=' expected at line ~S,column ~S."
+		  (lexem-row (second eq-variable))
+		  (lexem-column (second eq-variable))))))))
 
 (defun <constant> (ts)
   (let ((unsigned-integer (<unsigned-integer> ts)))
-    (if unsigned-integer
-	(list 'constant unsigned-integer)
-	(warn "ERROR: <UNSIGNED-INTEGER> expected."))))
+    (when unsigned-integer
+      (list '<constant> unsigned-integer))))
 
 (defun <constant-identifier> (ts)
   (let ((identifier (<identifier> ts)))
-    (if identifier
-	(list 'constant-identifier identifier)
-	(warn "ERROR: <IDENTIFIER> error."))))
+    (when identifier
+      (list '<constant-identifier> identifier))))
 
 (defun <variable-identifier>(ts)
   (let ((identifier (<identifier> ts)))
-    (if identifier
-	(list 'variable-identifier identifier)
-	(warn "ERROR: <IDENTIFIER> error."))))
+    (when identifier
+      (list '<variable-identifier> identifier))))
 
 (defun <procedure-identifier>(ts)
   (let ((identifier (<identifier> ts)))
-    (if identifier
-	(list 'procedure-identifier identifier)
-	(warn "ERROR: <IDENTIFIER> error."))))
+    (when identifier
+      (list '<procedure-identifier> identifier))))
 
 (defun <unsigned-integer>(ts)
   (if (is-correct-lexem ts 'delimeter 'minus)
       (let ((lexem (scan)))
 	(if (eq (car lexem) 'unsigned-integer)
 	    (let ((unsigned-integer (second lexem)))
-	      (list 'unsigned-integer
+	      (list '<unsigned-integer>
 		    (make-lexem :value (- (lexem-value unsigned-integer))
 				:row (lexem-row unsigned-integer)
 				:column (1- (lexem-column unsigned-integer)))))
-	    (warn "ERROR: <UNSIGNED-INTEGER> expected." )))
+	    (warn "ERROR: <UNSIGNED-INTEGER> expected at line ~S, column ~S."
+		  (lexem-row (second lexem))
+		  (lexem-column (second lexem)))))
       (if (eq (car ts) 'unsigned-integer)
-	  ts
-	  (warn "ERROR: <UNSIGNED-INTEGER> expected."))))
+	  (list '<unsigned-integer> (second ts))
+	  (warn "ERROR: <UNSIGNED-INTEGER> expected at line ~S, column ~S."
+		(lexem-row (second ts))
+		(lexem-column (second ts))))))
 
 (defun <identifier>(ts)
   (if (eq (car ts) 'identifier)
-	ts
-	(warn "ERROR: <IDENTIFIER> expected.")))
+      (list '<identifier> (second ts))
+      (warn "ERROR: <IDENTIFIER> expected at line ~S,column ~S."
+	    (lexem-row (second ts))
+	    (lexem-column (second ts)))))
